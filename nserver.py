96dc56fd653c4657bb4e118085bf4ecbf0f992eb
Nazmeen Khan <36809904+nazmeenkhan@users.noreply.github.com>
2019-02-23 19:08:47 +0530

    Add files via upload

---
+++ nserver.py
@@ -0,0 +1,62 @@
+import socket
+
+from Crypto.PublicKey import RSA
+from Crypto import Random
+from base64 import b64encode, b64decode
+import rsa
+
+keysize=1024
+random_generator = Random.new().read
+key = RSA.generate(keysize, random_generator)
+private, public= key, key.publickey()
+
+s = socket.socket()
+
+print ("Socket created")
+HOST = '127.0.0.1'  
+PORT = 5050
+s.bind((HOST,PORT))
+s.listen(5) 
+conn, address = s.accept()
+conn.send(public.exportKey())
+conn.send(private.exportKey())
+ch = int(conn.recv(1024))
+if ch==1:
+	while (True):
+		try:
+			msg=conn.recv(2048)
+			decryptor=(rsa.decrypt(b64decode(msg),private)).decode()
+			print(decryptor)
+			msgc= input('Enter message:').encode()
+			if msgc == b'Quit':	
+				conn.send(msgc)
+				conn.close()
+				break
+			else:
+				encrypted = b64encode(rsa.encrypt(msgc, public))
+				conn.send(encrypted)
+		except KeyboardInterrupt:
+			conn.close()
+			break
+elif ch==2:
+	print('Digital Signature')
+	msg = conn.recv(1024)
+	sign = conn.recv(1024)
+	verify = rsa.verify(msg, b64decode(sign),public)
+	print('Is the Sender Verified ?', verify)
+	conn.close()
+elif ch==3:
+	print('Encryption Decryption With Digital Signature')
+	msg = conn.recv(2048)
+	decryptor=rsa.decrypt(b64decode(msg),private)
+	sign = conn.recv(1024)
+	verify = rsa.verify(decryptor, b64decode(sign), public)
+	print('Is the Sender Verified ?', verify)
+	if verify:
+		print(decryptor.decode())
+	else:
+		print('User Is not verified')
+		conn.close()
+else:
+	s.close()				
+
---
+++ ucl.py
@@ -0,0 +1,54 @@
+import socket
+from Crypto.PublicKey import RSA
+from Crypto import Random
+from base64 import b64decode , b64encode 
+import rsa
+
+keysize=1024
+random_generator = Random.new().read
+key = RSA.generate(keysize, random_generator)
+private1, public1= key, key.publickey()
+
+c=socket.socket()
+HOST= '127.0.0.1'
+PORT = 5050
+c.connect((HOST, PORT))
+  
+public=rsa.importKey(c.recv(2048))
+private = rsa.importKey(c.recv(2048))
+print("1-encryption\n2-digital signature\n3-both\n4-quit")
+
+ch=input("enter your choice").encode()
+c.send(ch)
+if int(ch) == 1:
+
+	while True:
+		try:
+			msg = input('Enter message:').encode()
+			encrypted = b64encode(rsa.encrypt(msg,public))
+			c.send(encrypted)
+			msgc=c.recv(2048)
+			if msgc.decode() == 'Quit':
+				c.close()
+				break
+			else:
+				print(rsa.decrypt(b64decode(msgc), private).decode())
+		except KeyboardInterrupt:
+			c.close()
+			break
+elif int(ch)==2:
+
+	msg = input('Enter Message:').encode()
+	c.send(msg)
+	signature = b64encode(rsa.sign(msg, private))
+	c.send(signature)
+elif int(ch)==3:
+
+	print('Encryption Decryption With Digital Signature')
+	msg = input('Enter Message:').encode()
+	encrypted = b64encode(rsa.encrypt(msg,public))
+	c.send(encrypted)
+	signature = b64encode(rsa.sign(msg, private))
+	c.send(signature)
+else:
+	exit()
